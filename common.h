bf7c5816392c4858a1e63d077dfe985ccb0e0d00
CHENHONGHONG\宏鸿 <aspwebchh@hotmail.com>
2017-09-15 21:11:32 +0800

    [add]部分功能更新

--- common.h
+++ common.h
@@ -5,13 +5,30 @@
 
 using namespace std;
 
-enum TagType
-{
+enum TagType {
 	script = 1,
 	style = 2
+};
+
+enum UrlType{
+	withVersionNum = 1,
+	withParams = 2,
+	onlyUrl = 3
+};
+
+struct TagStruct
+{
+	string sourcePath;
+	string tagHtml;
+	TagType tagType;
+	string url;
+	string versionNum;
+	UrlType urlType;
 };
 
 bool exists(string val, string str);
 string fileContent(string path);
 void getAllFiles(string path, vector<string>& files);
 void saveFile(string content);
+string int2String(int value);
+string replaceAll(std::string str, const std::string from, const std::string to);
--- default.cpp
+++ default.cpp
@@ -8,8 +8,51 @@
 #include <iostream>
 #include <regex>
 #include <tuple>
+#include <time.h>
 
 using namespace std;
+
+const string SOURCE_VERSION = "source_version";
+
+string extractUrl(string tag) {
+	regex reg(".+?(src|href)=(\"|')(.+?)\\2.+");
+	smatch match;
+	string result;
+	if (regex_match(tag, match, reg)) {
+		result = match[match.size() - 1];
+	}
+	return result;
+}
+
+tuple<string, UrlType> extractVersionNum(string url) {
+	regex reg("\\.(js|css)\\?.+");
+	string result;
+	if (!regex_search(url, reg)) {
+		return{ result, onlyUrl };
+	}
+	regex reg1(".+?(\\?|&)"+ SOURCE_VERSION +"=([^&$]+).*");
+	smatch match;
+	if (!regex_match(url, match, reg1)) {
+		return{ result,withParams };
+	}
+	return{ match[match.size() - 1] , withVersionNum };
+}
+
+string newVersionNum() {
+	auto now = time(NULL);
+	auto tim = new tm();
+	localtime_s(tim, &now);
+	auto year = int2String(tim->tm_year + 1900);
+	auto month = int2String(tim->tm_mon + 1);
+	auto day = int2String(tim->tm_mday);
+	auto hour = int2String(tim->tm_hour);
+	auto min = int2String(tim->tm_min);
+	auto sec = int2String(tim->tm_sec);
+	delete tim;
+	return year + month + day + hour + min + day;
+}
+
+
 
 vector<string> findHtmlFiles(vector<string> allFiles) {
 	regex reg1(".+html$", regex_constants::icase);
@@ -23,71 +66,92 @@ vector<string> findHtmlFiles(vector<string> allFiles) {
 	return htmlFiles;
 }
 
-vector<tuple<string, TagType>> findTag( string htmlPath, string regexString, TagType tagType ) {
-	vector<tuple<string, TagType>> results;
-	string content = fileContent(htmlPath);
-	regex reg(regexString, regex_constants::icase);
-	for (sregex_iterator it(content.begin(), content.end(), reg), end; it != end; ++it) {
-		results.push_back({ it->str(), tagType });
+string genUrlWithVersionNum(string url, UrlType urlType) {
+	string newUrl;
+	regex reg(SOURCE_VERSION + "=[^&$]*");
+	auto nvn = newVersionNum();
+	switch (urlType)
+	{
+	case withVersionNum:
+		newUrl = regex_replace(url, reg, SOURCE_VERSION + "=" + newVersionNum());
+		break;
+	case withParams:
+		newUrl = url + "&" + SOURCE_VERSION + "=" + nvn;
+		break;
+	case onlyUrl:
+		newUrl = url + "?"+ SOURCE_VERSION +"=" + nvn;
+		break;
+	default:
+		break;
 	}
-	return results;
+	return newUrl;
 }
 
-vector<tuple<string, TagType>> mergeTags(vector<tuple<string, TagType>> v1, vector<tuple<string, TagType>> v2) {
-	vector<tuple<string, TagType>> v3 = {};
+void updateVersionNum(string filePath, string fileContent, vector<TagStruct> tagDataList) {
+	auto content = fileContent;
+	for (int i = 0; i < tagDataList.size(); i++) {
+		auto tagItem = tagDataList[i];
+		auto newUrl = genUrlWithVersionNum(tagItem.url, tagItem.urlType);
+		//auto newTag = replaceAll(tagItem.tagHtml, tagItem.url, newUrl);
+		//content = replaceAll(content, tagItem.url, newUrl);
+		//cout << content.find(tagItem.url, 0) << endl;
+		//cout << content.find(tagItem.url, 0) << endl;
+		// cout << content.find(tagItem.url, 0) << endl;
+		content.replace(content.find(tagItem.url, 0),tagItem.url.length(), newUrl);
+	}
+	saveFile(content);
+}
+
+vector<TagStruct> findTag(string htmlPath, string regexString, TagType tagType) {
+	string content = fileContent(htmlPath);
+	regex reg(regexString, regex_constants::icase);
+	vector<TagStruct> newResult;
+	for (sregex_iterator it(content.begin(), content.end(), reg), end; it != end; ++it) {
+		auto tag = it->str();
+
+		TagStruct tagStruct;
+		tagStruct.sourcePath = htmlPath;
+		tagStruct.tagHtml = tag;
+		tagStruct.tagType = tagType;
+		tagStruct.url = extractUrl(tag);
+
+		auto versionNumInfo = extractVersionNum(tagStruct.url);
+		tagStruct.versionNum = get<0>(versionNumInfo);
+		tagStruct.urlType = get<1>(versionNumInfo);
+
+		newResult.push_back(tagStruct);
+	}
+	updateVersionNum(htmlPath, content, newResult);
+	return newResult;
+}
+
+vector<TagStruct> mergeTags(vector<TagStruct> v1, vector<TagStruct> v2) {
+	vector<TagStruct> v3 = {};
 	v3.insert(v3.end(), v1.begin(), v1.end());
 	v3.insert(v3.end(), v2.begin(), v2.end());
 	return v3;
 }
 
-vector<tuple<string,TagType>> findTag(string path) {
+vector<TagStruct> findTag(string path) {
 	auto scriptTags = findTag(path, "<script.+?<\/script>", script);
 	auto linkTags = findTag(path, "<link[^>]+>", style);
 	auto tags = mergeTags(scriptTags, linkTags);
 	return tags;
 }
 
-string extractUrl(string tag) {
-	regex reg(".+?(src|href)=(\"|')(.+?)\\2.+");
-	smatch match;
-	string result;
-	if (regex_match(tag, match, reg)) {
-		result = match[match.size() - 1];
-	}
-	return result;
-}
-
-string extractVersionNum(string url) {
-	regex reg("\\.(js|css)\\?.+");
-	string result;
-	if (!regex_search(url, reg)) {
-		return result;
-	}
-	regex reg1(".+?(\\?|&)source_version=([^&$]+).*");
-	smatch match;
-	if ( !regex_match(url,match,reg1)) {
-		return result;
-	}
-	return match[match.size()-1];
-}
 
 
 int main() {
-	//auto versionNum = extractVersionNum("abc.js?source_version=2");
-	//cout << versionNum << endl;
-	//cin.get();
 	vector<string> files;
 	getAllFiles("C:\\Users\\宏鸿\\Desktop\\手游社区\\in-game Community (1)", files);
-	vector<string> htmlPaths = findHtmlFiles(files);
+	auto htmlPaths = findHtmlFiles(files);
 	for (int i = 0; i < htmlPaths.size(); i++) {
-		string path = htmlPaths[i];
-		if (exists("test.html",path)){
+		auto path = htmlPaths[i];
+		if (exists("test.html", path)) {
 			auto tags = findTag(path);
 			for (int i = 0; i < tags.size(); i++) {
-				auto tag = get<0>(tags[i]);
-				auto tagType = get<1>(tags[i]);
-				auto url = extractUrl(tag);
-				cout << url << endl;
+				auto item = tags[i];
+				//cout << item.url << "|" << item.versionNum << "|" << item.urlType << endl;
 			}
 		}
 	}
